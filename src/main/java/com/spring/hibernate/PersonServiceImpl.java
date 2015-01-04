01f40a4f11e7172718f14f8d2c679bb2c9fa133e
Tarun <tarun.softengg@gmail.com>
2015-01-04 23:19:52 +0530

    initial-commit

---
+++ src/main/java/com/spring/hibernate/PersonServiceImpl.java
@@ -0,0 +1,48 @@
+package com.spring.hibernate;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonServiceImpl implements PersonService {
+     
+    private PersonDAO personDAO;
+ 
+    public void setPersonDAO(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+ 
+    @Override
+    @Transactional
+    public void addPerson(Person p) {
+        this.personDAO.addPerson(p);
+    }
+ 
+    @Override
+    @Transactional
+    public void updatePerson(Person p) {
+        this.personDAO.updatePerson(p);
+    }
+ 
+    @Override
+    @Transactional
+    public List<Person> listPersons() {
+        return this.personDAO.listPersons();
+    }
+ 
+    @Override
+    @Transactional
+    public Person getPersonById(int id) {
+        return this.personDAO.getPersonById(id);
+    }
+ 
+    @Override
+    @Transactional
+    public void removePerson(int id) {
+        this.personDAO.removePerson(id);
+    }
+ 
+}
